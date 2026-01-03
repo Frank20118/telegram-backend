cd95e5318adba42ab1bea4b6001343ee28d83127
Frank20118 <axelbolt202277@gmail.com>
2026-01-03 22:20:45 +0500

    Update auth.controller.ts

--- auth/auth.controller.ts
+++ auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
+
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
@@ -15,3 +16,4 @@ export class AuthController {
     return this.authService.login(body.email, body.password);
   }
 }
+
