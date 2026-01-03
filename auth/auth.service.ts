7a6ddf9a00d20cc06e274f3e2224608d4c06c27a
Your Name <your-email@example.com>
2026-01-03 12:43:16 +0500

    Initial backend commit

---
+++ auth/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from '../users/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async createUser(email: string, username: string, password: string) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = this.usersRepository.create({ email, username, password: hashedPassword });
+    return this.usersRepository.save(user);
+  }
+
+  async findByEmail(email: string) {
+    return this.usersRepository.findOne({ where: { email } });
+  }
+
+  async findByUsername(username: string) {
+    return this.usersRepository.findOne({ where: { username } });
+  }
+
+  async searchByUsername(query: string) {
+    return this.usersRepository
+      .createQueryBuilder('user')
+      .where('user.username ILIKE :query', { query: `%${query}%` })
+      .getMany();
+  }
+
+  async updateUsername(userId: number, newUsername: string) {
+    await this.usersRepository.update({ id: userId }, { username: newUsername });
+    return this.usersRepository.findOne({ where: { id: userId } });
+  }
+}
