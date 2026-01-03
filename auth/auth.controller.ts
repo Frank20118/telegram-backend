b90c9db594acf49eb399d43d9b7e5fdb4804a9fe
Frank20118 <axelbolt202277@gmail.com>
2026-01-03 23:18:32 +0500

    Update auth.controller.ts

--- auth/auth.controller.ts
+++ auth/auth.controller.ts
@@ -1,19 +1,20 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  async register(@Body() body: { email: string; username: string; password: string }) {
-    return this.authService.register(body.email, body.username, body.password);
-  }
-
-  @Post('login')
-  async login(@Body() body: { email: string; password: string }) {
-    return this.authService.login(body.email, body.password);
+  async register(
+    @Body() body: { email: string; username: string; password: string },
+  ) {
+    return this.authService.createUser(
+      body.email,
+      body.username,
+      body.password,
+    );
   }
 }
+
 
