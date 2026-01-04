e667232851f20f73924e1fe9fa94c4995baa415a
Frank20118 <axelbolt202277@gmail.com>
2026-01-04 15:53:37 +0500

    Update chat.entity.ts

--- src/chats/chat.entity.ts
+++ src/chats/chat.entity.ts
@@ -1,10 +1,29 @@
-import { User } from '../users/user.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToMany,
+  OneToMany,
+  JoinTable,
+} from 'typeorm';
+import { User } from '../../users/user.entity';
 import { Message } from './message.entity';
 
+@Entity()
 export class Chat {
+  @PrimaryGeneratedColumn()
   id!: number;
+
+  @Column()
   type!: 'private' | 'group' | 'channel';
+
+  @Column({ nullable: true })
   title!: string;
+
+  @ManyToMany(() => User, user => user.chats)
+  @JoinTable()
   members!: User[];
+
+  @OneToMany(() => Message, message => message.chat)
   messages!: Message[];
 }
