561ad94631902da4cf8dcde9f547665ff227122e
Frank20118 <axelbolt202277@gmail.com>
2026-01-04 12:01:12 +0500

    Update media.controller.ts

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -6,23 +6,19 @@ import { v4 as uuid } from 'uuid';
 
 @Controller('media')
 export class MediaController {
-
   @Post('upload')
   @UseInterceptors(
     FileInterceptor('file', {
       storage: diskStorage({
         destination: './uploads',
         filename: (req: any, file: Express.Multer.File, cb: any) => {
-          const randomName = `${uuid()}${extname(file.originalname)}`;
-          cb(null, randomName);
-        }
-      })
-    })
+          const uniqueSuffix = `${uuid()}${extname(file.originalname)}`;
+          cb(null, uniqueSuffix);
+        },
+      }),
+    }),
   )
   uploadFile(@UploadedFile() file: Express.Multer.File) {
-    return {
-      filename: file.filename,
-      path: file.path
-    };
+    return { filename: file.filename, path: file.path };
   }
 }
