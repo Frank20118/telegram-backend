42282ab5654b9c09468a716526d67c7c76b1ff8f
Frank20118 <axelbolt202277@gmail.com>
2026-01-05 00:27:23 +0500

    Update user.entity.ts

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,24 +1,10 @@
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  OneToMany,
-} from 'typeorm';
-import { Chat } from '../chats/chat.entity';
-import { Message } from '../chats/message.entity';
-
-@Entity()
 export class User {
-  @PrimaryGeneratedColumn()
   id: number;
-
-  @Column({ unique: true })
-  username: string;
-
-  @OneToMany(() => Chat, chat => chat.owner)
-  chats: Chat[];
-
-  @OneToMany(() => Message, message => message.sender)
-  messages: Message[];
+  name: string;
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+  }
 }
+
 
