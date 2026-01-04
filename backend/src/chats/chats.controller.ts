c662ef7e2c9bdcc197532ee9643bd3489e296b57
Frank20118 <axelbolt202277@gmail.com>
2026-01-04 15:27:57 +0500

    Update chats.controller.ts

--- backend/src/chats/chats.controller.ts
+++ backend/src/chats/chats.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { ChatsService } from './chats.service';
+import { Chat } from './chat.entity';
 
 @Controller('chats')
 export class ChatsController {
@@ -7,10 +8,15 @@ export class ChatsController {
 
   @Post()
   async createChat(
-    @Body('type') type: string,
+    @Body('type') type: 'private' | 'group' | 'channel',
     @Body('title') title: string,
     @Body('members') members: number[],
-  ) {
+  ): Promise<Chat> {
     return this.chatsService.createChat(type, title, members);
+  }
+
+  @Get(':id')
+  async getChatById(@Param('id') id: number): Promise<Chat | undefined> {
+    return this.chatsService.getChatById(Number(id));
   }
 }
