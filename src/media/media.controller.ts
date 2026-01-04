29d1dfaee9754499638eb0c65a78480f51bb455a
Frank20118 <axelbolt202277@gmail.com>
2026-01-04 11:51:13 +0500

    Update media.controller.ts

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -1,19 +1,28 @@
 import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
-import { v4 as uuid } from 'uuid';
 import { extname } from 'path';
+import { v4 as uuid } from 'uuid';
 
 @Controller('media')
 export class MediaController {
+
   @Post('upload')
-  @UseInterceptors(FileInterceptor('file', {
-    storage: diskStorage({
-      destination: './uploads',
-      filename: (req, file, cb) => cb(null, `${uuid()}${extname(file.originalname)}`)
-    }),
-  }))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req: any, file: Express.Multer.File, cb: any) => {
+          const randomName = `${uuid()}${extname(file.originalname)}`;
+          cb(null, randomName);
+        }
+      })
+    })
+  )
   uploadFile(@UploadedFile() file: Express.Multer.File) {
-    return { url: `/uploads/${file.filename}`, originalName: file.originalname };
+    return {
+      filename: file.filename,
+      path: file.path
+    };
   }
 }
