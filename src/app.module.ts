d2cc38c150d76e3abcdef7b0c5c886f55305434a
Frank20118 <axelbolt202277@gmail.com>
2026-01-04 19:05:35 +0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/user.entity';
-import { Chat } from './gateway/chats/chat.entity';
-import { Message } from './gateway/chats/message.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      entities: [User, Chat, Message],
+      type: 'sqlite', 
+      database: 'data.sqlite',
       synchronize: true,
-    }),
+      autoLoadEntities: true
+    })
   ],
+  controllers: [],
+  providers: []
 })
 export class AppModule {}
